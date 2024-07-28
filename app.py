d04afe1c820a35f241068c1e288b209715b0d58b
pcbotz <jerinpc273@gmail.com>
2024-07-28 21:31:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, redirect, url_for, render_template, flash
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# MongoDB setup
+client = MongoClient("mongodb+srv://web:web@cluster0.poowfhi.mongodb.net/web?retryWrites=true&w=majority&appName=Cluster0")
+db = client.web
+
+@app.route('/')
+def home():
+    updates = db.updates.find()
+    return render_template('index.html', updates=updates)
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        password = request.form['password']
+        if password == 'pcsdfgjij345jiorigjjv':
+            return redirect(url_for('add_update'))
+        else:
+            flash('Incorrect password', 'error')
+    return render_template('admin.html')
+
+@app.route('/add_update', methods=['GET', 'POST'])
+def add_update():
+    if request.method == 'POST':
+        photo = request.form['photo']
+        text = request.form['text']
+        link = request.form['link']
+        db.updates.insert_one({'photo': photo, 'text': text, 'link': link})
+        flash('Update added successfully', 'success')
+        return redirect(url_for('home'))
+    return render_template('add_update.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
