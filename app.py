19ab550dd9d72124e9d58ff472334a2db5d55726
PC <126058657+pcbotz@users.noreply.github.com>
2024-08-03 09:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
 from pymongo import MongoClient
-from bson.objectid import ObjectId  # Import ObjectId to work with MongoDB IDs
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -11,8 +11,12 @@ db = client.web
 
 @app.route('/')
 def home():
-    updates = db.updates.find()
-    return render_template('index.html', updates=updates)
+    return render_template('index.html')
+
+@app.route('/updates')
+def updates():
+    updates = db.updates.find().sort("_id", -1)  # Sort by ID in descending order
+    return render_template('updates.html', updates=updates)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
@@ -32,15 +36,14 @@ def add_update():
         link = request.form['link']
         db.updates.insert_one({'photo': photo, 'text': text, 'link': link})
         flash('Update added successfully', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('updates'))
     return render_template('add_update.html')
 
 @app.route('/delete_update/<update_id>', methods=['POST'])
 def delete_update(update_id):
-    if request.method == 'POST':
-        db.updates.delete_one({'_id': ObjectId(update_id)})
-        flash('Update deleted successfully', 'success')
-    return redirect(url_for('home'))
+    db.updates.delete_one({'_id': ObjectId(update_id)})
+    flash('Update deleted successfully', 'success')
+    return redirect(url_for('updates'))
 
 if __name__ == '__main__':
     app.run(debug=True)
