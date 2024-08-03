3b0ecc0657e820b02b89c83ca0694446f7d2bce1
PC <126058657+pcbotz@users.noreply.github.com>
2024-08-03 09:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -9,20 +9,19 @@ app.secret_key = 'your_secret_key'
 client = MongoClient("mongodb+srv://web:web@cluster0.poowfhi.mongodb.net/web?retryWrites=true&w=majority&appName=Cluster0")
 db = client.web
 
+# Store hashed password for security
+admin_password_hash = generate_password_hash('sdfe53rf564gdfgerh')
+
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/updates')
-def updates():
-    updates = db.updates.find().sort("_id", -1)  # Sort by ID in descending order
-    return render_template('updates.html', updates=updates)
+    updates = db.updates.find()
+    return render_template('index.html', updates=updates)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if request.method == 'POST':
         password = request.form['password']
-        if password == 'pcsdfgjij345jiorigjjv':
+        if check_password_hash(admin_password_hash, password):
             return redirect(url_for('add_update'))
         else:
             flash('Incorrect password', 'error')
@@ -36,13 +35,22 @@ def add_update():
         link = request.form['link']
         db.updates.insert_one({'photo': photo, 'text': text, 'link': link})
         flash('Update added successfully', 'success')
-        return redirect(url_for('updates'))
+        return redirect(url_for('home'))
     return render_template('add_update.html')
+
+@app.route('/updates')
+def updates():
+    updates = db.updates.find()
+    return render_template('updates.html', updates=updates)
 
 @app.route('/delete_update/<update_id>', methods=['POST'])
 def delete_update(update_id):
-    db.updates.delete_one({'_id': ObjectId(update_id)})
-    flash('Update deleted successfully', 'success')
+    password = request.form.get('password')
+    if check_password_hash(admin_password_hash, password):
+        db.updates.delete_one({'_id': ObjectId(update_id)})
+        flash('Update deleted successfully', 'success')
+    else:
+        flash('Incorrect password', 'error')
     return redirect(url_for('updates'))
 
 if __name__ == '__main__':
